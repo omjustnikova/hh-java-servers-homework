8d9d6367786595a93159a3fa492a689730bebc18
Kirill Vasilev <ki.vasilyev@hh.ru>
2022-10-19 00:11:28 +0600

    add test framework

---
+++ jersey/src/test/java/JerseyAppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JerseyAppTest {
+
+  @Test
+  public void mainTest() {
+    Assertions.assertTrue(true);
+  }
+
+}
---
+++ servlet/src/test/java/ServletApplicationTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ServletApplicationTest {
+
+  @Test
+  public void mainTest() {
+    Assertions.assertTrue(true);
+  }
+
+}
