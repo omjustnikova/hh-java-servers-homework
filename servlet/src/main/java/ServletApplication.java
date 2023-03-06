9114f572d6505e1311414960110640adc43ed670
omjustnikova <omjustnikova@gmail.com>
2023-03-06 16:23:25 +0600

    The homework was done

--- servlet/src/main/java/ServletApplication.java
+++ servlet/src/main/java/ServletApplication.java
@@ -1,6 +1,22 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
 public class ServletApplication {
 
-  public static void main(String[] args) {
-    // run, Jetty, run!
+  private static Server createServer(int port) {
+    Server server = new Server(port);
+    ServletContextHandler handler = new ServletContextHandler(server, "/");
+    handler.addServlet(StatusServlet.class, "/status");
+    handler.addServlet(CounterServlet.class, "/counter");
+    handler.addServlet(CounterClearServlet.class, "/counter/clear");
+    server.setHandler(handler);
+    return server;
+  }
+
+  public static void main(String[] args) throws Exception {
+    int port = 8081;
+    Server server = createServer(port);
+    server.start();
+    server.join();
   }
 }
