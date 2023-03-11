71e0fedb02bdeb62a25c269322d687739f99b359
omjustnikova <omjustnikova@gmail.com>
2023-03-11 21:13:35 +0600

    The code was refactored

--- servlet/src/main/java/CounterClearServlet.java
+++ servlet/src/main/java/CounterClearServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 
 @WebServlet(urlPatterns = "/counter/clear")
@@ -13,19 +14,34 @@ public class CounterClearServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO: need refactoring, but I don't know how to do it. Let me time to think more please
-        if (request.getCookies() == null) {
-            response.setStatus(412);
-        } else {
-            Optional<Cookie> authCookie = Stream.of(request.getCookies())
-                    .filter(cookie -> cookie.getName().equals("hh-auth"))
-                    .findFirst();
-            if (authCookie.isPresent() && authCookie.get().getValue() != null && authCookie.get().getValue().length() > 10) {
-                CounterDAO.getInstance().clearCounter();
-            } else {
-                // Precondition Failed
-                response.setStatus(412);
+        //TODO: simplify the code if it is possible
+        boolean clearCounterIsSuccessful = false;
+
+        if (request.getCookies() != null) {
+
+            Optional<Cookie> cookie =
+                    Stream.of(request.getCookies())
+                            .filter(cookieVal -> cookieVal.getName().equals("hh-auth"))
+                            .findFirst();
+
+            if (cookie.isPresent()) {
+
+                Optional<String> cookieValue =
+                        Optional.ofNullable(cookie.get().getValue());
+
+                if (cookieValue.isPresent() && cookieValue.get().length() > 10) {
+                    CounterDAO.getInstance().clearCounter();
+                    clearCounterIsSuccessful = true;
+                }
             }
         }
+
+        if (!clearCounterIsSuccessful) {
+            setPreconditionFailedResponseStatus(response);
+        }
+    }
+
+    private void setPreconditionFailedResponseStatus(HttpServletResponse response) {
+        response.setStatus(412);
     }
 }
