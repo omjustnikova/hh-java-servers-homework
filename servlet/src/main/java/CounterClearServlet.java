39b1ee3c0016662c51d25fa6c08539cd8b6c50f7
omjustnikova <omjustnikova@gmail.com>
2023-03-12 20:13:39 +0600

    Explanation comments were added

--- servlet/src/main/java/CounterClearServlet.java
+++ servlet/src/main/java/CounterClearServlet.java
@@ -6,32 +6,30 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Stream;
 
 @WebServlet(urlPatterns = "/counter/clear")
 public class CounterClearServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Optional<String> hhAuthCookieValue = Optional.ofNullable(request)
-                .flatMap(requestObj -> Optional.ofNullable(requestObj.getCookies()))
-                .stream()
-                .flatMap(Arrays::stream)
-                .filter(cookie -> Objects.equals("hh-auth", cookie.getName()))
-                .findFirst()
+        Optional<String> hhAuthCookieValue = Optional.ofNullable(request) // NPE in case request is null was fixed
+                .flatMap(requestObj -> Optional.ofNullable(requestObj.getCookies())) // NPE in case getCookies() is null was fixed
+                .stream() // Stream<Cookie[]>
+                .flatMap(Arrays::stream) // Stream<Cookie>
+                .filter(cookie -> Objects.equals("hh-auth", cookie.getName())) // cookie.getName().equals("hh-auth") can throw NPE in case getName is null
+                .findFirst() // in case findFirst is the first there is no NPE, otherwise(map is the first) it is 
                 .map(Cookie::getValue);
 
 
             if (hhAuthCookieValue.isEmpty() || hhAuthCookieValue.get().length() <= 10) {
-                setPreconditionFailedResponseStatus(response);
+                setUnauthorizedResponseStatus(response);
                 return;
             }
 
             CounterDAO.getInstance().clearCounter();
     }
 
-    private void setPreconditionFailedResponseStatus(HttpServletResponse response) {
-        response.setStatus(412);
+    private void setUnauthorizedResponseStatus(HttpServletResponse response) {
+        response.setStatus(401);
     }
 }
