66da126c92dc20ef6cf0952220a36e900ec3aa17
omjustnikova <omjustnikova@gmail.com>
2023-03-18 20:01:39 +0600

    javax.ws.rs.core.Application is used now

--- jersey/src/main/java/JerseyApplication.java
+++ jersey/src/main/java/JerseyApplication.java
@@ -3,23 +3,42 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
-import resource.CounterResource;
-import resource.StatusResource;
 
-public class JerseyApplication {
+public class JerseyApplication extends ResourceConfig {
+
+    public JerseyApplication() {
+        packages("resource");
+    }
 
     private static Server createServer(int port) {
         Server server = new Server(port);
 
-        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-        server.setHandler(handler);
-        handler.setContextPath("/");
+///////// 1 way
+//        ServletContextHandler handler = new ServletContextHandler();
+//        server.setHandler(handler);
+//
+//        ServletHolder servletHolder = handler.addServlet(ServletContainer.class, "/*");
+//        servletHolder.setInitOrder(0);
+//        servletHolder.setInitParameter("jersey.config.server.provider.packages", "resource");
+////////// end of 1 way
 
-        ResourceConfig resourceConfig = new ResourceConfig();
-        resourceConfig.register(StatusResource.class);
-        resourceConfig.register(CounterResource.class);
+////////// 2 way
+//        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+//        server.setHandler(handler);
+//        handler.setContextPath("/");
+//
+//        ResourceConfig resourceConfig = new ResourceConfig();
+//        resourceConfig.register(StatusResource.class);
+//        resourceConfig.register(CounterResource.class);
+//
+//        handler.addServlet(new ServletHolder(new ServletContainer(resourceConfig)), "/*");
+///////// end of 2 way
 
-        handler.addServlet(new ServletHolder(new ServletContainer(resourceConfig)), "/*");
+        ServletContextHandler servletContextHandler = new ServletContextHandler();
+        ServletHolder servletHolder = servletContextHandler.addServlet(ServletContainer.class, "/*");
+        servletHolder.setInitParameter("jakarta.ws.rs.Application", JerseyApplication.class.getName());
+
+        server.setHandler(servletContextHandler);
 
         return server;
     }
